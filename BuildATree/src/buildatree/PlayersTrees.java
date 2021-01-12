80d6227c01e98641ffdb53cfcc38633a1f8551ec
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2021-01-11 20:25:39 -0600

    Cambios

--- BuildATree/src/buildatree/PlayersTrees.java
+++ BuildATree/src/buildatree/PlayersTrees.java
@@ -4,12 +4,23 @@ public class PlayersTrees {
 
     String arbol;
 
+    Player player1 = new Player();
+
+    Player player2 = new Player();
+
+    Player player3 = new Player();
+
+    Player player4 = new Player();
+
     public void setArbol(String data){
         arbol = data;
     }
 
     public void comenzar(int numeroJugadores){
         if (numeroJugadores == 2){
+            player1.setArbol(arbol);
+            player2.setArbol(arbol);
+
             if (arbol.equals("AVL")){
 
             }
@@ -25,11 +36,16 @@ public class PlayersTrees {
         }
 
         else if (numeroJugadores == 3){
-
+            player1.setArbol(arbol);
+            player2.setArbol(arbol);
+            player3.setArbol(arbol);
         }
 
         else{
-
+            player1.setArbol(arbol);
+            player2.setArbol(arbol);
+            player3.setArbol(arbol);
+            player4.setArbol(arbol);
         }
     }
 }
